b4edf0a06a4c186e79e1103384ae4494ad1fc3ff
Matthias Müller <matthias.mueller@trivadis.com>
2019-07-29 08:27:06 +0200

    WIP

--- src/areas/spec-deletion/services/implementation/spec-deletion.service.ts
+++ src/areas/spec-deletion/services/implementation/spec-deletion.service.ts
@@ -5,5 +5,8 @@ import { ISpecDeletionService } from '../spec-deletion-service.interface';
 @injectable()
 export class SpecDeletionService implements ISpecDeletionService {
     public deleAllSpecFiles(): void {
+        // 1. Get All files to delete
+        // 2. Prompt if sure with amount of files to be deleted
+        // 3. If yes, delete files
     }
 }
