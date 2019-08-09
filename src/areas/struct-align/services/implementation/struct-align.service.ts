f73d7ade1250f88018ec5685ff75fce60de0f4fc
Matthias Müller <matthias.mueller@trivadis.com>
2019-08-09 14:58:29 +0200

    WIP

--- src/areas/struct-align/services/implementation/struct-align.service.ts
+++ src/areas/struct-align/services/implementation/struct-align.service.ts
@@ -11,31 +11,36 @@ export class StructAlignService implements IStructAlignService {
   }
 
   public alignTextMarks(textEditor: TextEditor, edit: TextEditorEdit): void {
-    const placeHolder = '%.';
+    const placeHolder = '%%';
 
     const namespace = this.getSeparatedRelativeNamespace(textEditor.document);
 
     const fullText = textEditor.document.getText();
     const replaceOccurences = fullText.split(placeHolder).length - 1;
-    this.infoService.showInfo(`Replacing ${ replaceOccurences } entries..`);
+    this.infoService.showInfo(`Replacing ${replaceOccurences} entries..`);
 
     const replacedText = fullText.split(placeHolder).join(namespace);
     const allTextRange = new Range(0, 0, textEditor.document.lineCount, fullText.length);
     edit.replace(allTextRange, replacedText);
 
-    this.infoService.showInfo(`Replaced ${ replaceOccurences } entries!`);
+    this.infoService.showInfo(`Replaced ${replaceOccurences} entries!`);
   }
 
   private getSeparatedRelativeNamespace(textDocument: TextDocument): string {
-    let relativePath = workspace.asRelativePath(textDocument.uri.fsPath);
-    const fileName = relativePath.split('/').pop()!;
-    relativePath = relativePath.replace(fileName, '');
+    // We can't use the workspace path, as we don't know, from which folder the user opens
+    // we search therefore for src/app/ and take this one as starting point
+    let relativePath = textDocument.uri.fsPath;
+    const pathSeparator = relativePath.indexOf('/') > -1 ? '/' : '\\';
 
-    if (relativePath.startsWith('app/')) {
-      relativePath = relativePath.substring(4);
+    const startIndex = relativePath.indexOf(`src${pathSeparator}app${pathSeparator}`);
+    if (startIndex > -1) {
+      relativePath = relativePath.substring(startIndex + 8);
     }
 
-    const pointSeparatedPath = relativePath.split('/').join('.');
+    const fileName = relativePath.split(pathSeparator).pop()!;
+    relativePath = relativePath.replace(fileName, '');
+
+    const pointSeparatedPath = relativePath.split(pathSeparator).join('.');
     return pointSeparatedPath;
   }
 }
